16e3b1ebc1febad3404d8229a93579976a8e9fa0
Snivy Films <mill385@yahoo.com>
2024-10-11 11:35:53 -0400

    Added Starting Candy Ability

---
+++ SnivysCustomRolesAbilities/Abilities/GivingCandyAbility.cs
@@ -0,0 +1,37 @@
+﻿using Exiled.API.Features.Attributes;
+using System.Collections.Generic;
+using Exiled.API.Features;
+using Exiled.CustomRoles.API.Features;
+using Exiled.Events.EventArgs.Player;
+using InventorySystem.Items.Usables.Scp330;
+using MEC;
+using PlayerRoles;
+using YamlDotNet.Core.Events;
+
+namespace SnivysCustomRolesAbilities.Abilities
+{
+    [CustomAbility]
+    public class GivingCandyAbility : PassiveAbility
+    {
+        public override string Name { get; set; } = "Giving Candy Ability";
+
+        public override string Description { get; set; } =
+            "Handles giving candy to a player at the beginning of the round";
+
+        public List<CandyKindID> StartingCandy { get; set; } = new List<CandyKindID>()
+        {
+            CandyKindID.Pink
+        };
+
+        protected override void AbilityAdded(Player player)
+        {
+            Timing.CallDelayed(1.0f, () =>
+            {
+                foreach (var candy in StartingCandy)
+                {
+                    player.TryAddCandy(candy);
+                }
+            });
+        }
+    }
+}
