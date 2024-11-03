547fd0e06fcac4ce1ad0520f4a95fea1630d33ae
Snivy Films <120346554+NikkiGardiner1@users.noreply.github.com>
2024-11-03 17:04:18 -0500

    Added an on spawn handle for stuff like custom escape points

--- SnivysCustomRolesAbilities/Abilities/CustomRoleEscape.cs
+++ SnivysCustomRolesAbilities/Abilities/CustomRoleEscape.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using Exiled.API.Features.Attributes;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using Exiled.API.Enums;
 using Exiled.API.Features;
 using Exiled.API.Features.Items;
 using Exiled.CustomRoles;
 using Exiled.CustomRoles.API.Features;
 using Exiled.Events.EventArgs.Player;
 using MEC;
+using PlayerRoles;
 
 namespace SnivysCustomRolesAbilities.Abilities
 {
@@ -24,32 +27,40 @@ namespace SnivysCustomRolesAbilities.Abilities
         public bool AllowUncuffedCustomRoleChange { get; set; } = true;
         public bool AllowCuffedCustomRoleChange { get; set; } = true;
         public bool SaveInventory { get; set; } = true;
+        [Description("Should OnSpawn be used if the player is uncuffed. Useful if there is custom escapes.")]
+        public bool UseOnSpawnUncuffedEscape { get; set; } = false;
+        [Description("Should OnSpawn be used if the player is cuffed. Useful if there is custom escapes.")]
+        public bool UseOnSpawnCuffedEscape { get; set; } = false;
 
+        private List<Item> storedInventory = new List<Item>();
+        
         
         protected override void AbilityAdded(Player player)
         {
             PlayersWithCustomRoleEscape.Add(player);
             Exiled.Events.Handlers.Player.Escaping += OnEscaping;
+            Exiled.Events.Handlers.Player.ChangingRole += OnRoleChange;
         }
         protected override void AbilityRemoved(Player player)
         {
             PlayersWithCustomRoleEscape.Remove(player);
             Exiled.Events.Handlers.Player.Escaping -= OnEscaping;
+            Exiled.Events.Handlers.Player.ChangingRole -= OnRoleChange;
         }
 
         private void OnEscaping(EscapingEventArgs ev)
         {
             if (!PlayersWithCustomRoleEscape.Contains(ev.Player))
                 return;
-            List<Item> storedInventory = ev.Player.Items.ToList();
+            storedInventory = ev.Player.Items.ToList();
             
-            if (ev.Player.IsCuffed && AllowCuffedCustomRoleChange && CuffedEscapeCustomRole != String.Empty)
+            if (ev.Player.IsCuffed && AllowCuffedCustomRoleChange && CuffedEscapeCustomRole != String.Empty && !UseOnSpawnCuffedEscape)
             {
                 ev.IsAllowed = false;
                 CustomRole.Get(CuffedEscapeCustomRole).AddRole(ev.Player);
                 storedInventory.Clear();
             }
-            else if (AllowUncuffedCustomRoleChange && UncuffedEscapeCustomRole != String.Empty)
+            else if (AllowUncuffedCustomRoleChange && UncuffedEscapeCustomRole != String.Empty && !UseOnSpawnUncuffedEscape)
             {
                 ev.IsAllowed = false;
                 CustomRole.Get(UncuffedEscapeCustomRole).AddRole(ev.Player);
@@ -66,6 +77,37 @@ namespace SnivysCustomRolesAbilities.Abilities
                 }
                 else
                     storedInventory.Clear();
+            }
+        }
+
+        private void OnRoleChange(ChangingRoleEventArgs ev)
+        {
+            if (ev.NewRole == RoleTypeId.Spectator && !PlayersWithCustomRoleEscape.Contains(ev.Player))
+                return;
+            if (UseOnSpawnUncuffedEscape && ev.Reason == SpawnReason.Escaped && UncuffedEscapeCustomRole != String.Empty)
+            {
+                ev.SpawnFlags = RoleSpawnFlags.UseSpawnpoint;
+                ev.Player.ClearInventory();
+                CustomRole.Get(UncuffedEscapeCustomRole).AddRole(ev.Player);
+                if (SaveInventory)
+                {
+                    Timing.CallDelayed(1f, () =>
+                    {
+                        foreach (Item item in storedInventory)
+                        {
+                            item.CreatePickup(ev.Player.Position);
+                        }
+                        storedInventory.Clear();
+                    });
+                }
+                else
+                    storedInventory.Clear();
+            }
+            else if (UseOnSpawnCuffedEscape && ev.Reason == SpawnReason.Escaped && CuffedEscapeCustomRole != String.Empty)
+            {
+                ev.SpawnFlags = RoleSpawnFlags.UseSpawnpoint;
+                CustomRole.Get(CuffedEscapeCustomRole).AddRole(ev.Player);
+                storedInventory.Clear();
             }
         }
     }
